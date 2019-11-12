27d3ebd9ca26ebdb7aa1783ff768811ed897fcac
darkenrule <fernandojesus.gonzalezalonso@gmail.com>
2019-11-12 08:21:54 -0600

    observaciones

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bioterio.db'
 db = SQLAlchemy(app)
 cepa_list = ['C57B6/J','CD45.1','RAG1+/-']
 
+
+#############################################TABLAS###############################################################
 class Cruza(db.Model):
     __tablename__ = 'cruza'
     __table_args__ = {'extend_existing': True}  
@@ -16,13 +18,15 @@ class Cruza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     caja = db.Column(db.String(20), nullable=False)
     cepa = db.Column(db.String(20), nullable=False)
-    fecha_cruza = db.Column(db.DateTime, default=datetime.utcnow)    
+    fecha_cruza = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)    
     machos = db.Column(db.Integer, default=0)
     hembras = db.Column(db.Integer, default=0)
     camadas = db.relationship('Camada', backref='cruza', lazy=True)
+    macho = db.relationship('Macho', backref='cruza', lazy=True)
+    hembra = db.relationship('Hembra', backref='cruza', lazy=True)
     
     def __repr__(self):
-        return '<Task %r>' %self.id
+        return '<Cruza %r>' %self.id
     
 class Camada(db.Model):
     __tablename__ = 'camada'
@@ -30,14 +34,69 @@ class Camada(db.Model):
     
     db.Model.metadata.reflect(db.engine)
     id = db.Column(db.Integer, primary_key=True)
-    fecha_nacimiento = db.Column(db.DateTime, default=datetime.utcnow)
-    fecha_destete = db.Column(db.DateTime, default=datetime.today() + relativedelta(days=+28))
-    machos = db.Column(db.Integer, default=0)
-    hembras = db.Column(db.Integer, default=0)
-    is_created = db.Column(db.Boolean, default=False)
+    fecha_nacimiento = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    fecha_destete = db.Column(db.DateTime, default=datetime.today() + relativedelta(days=+28), nullable=False)
+    machos = db.Column(db.Integer, default=0, nullable=False)
+    hembras = db.Column(db.Integer, default=0, nullable=False)
+    macho_is_created = db.Column(db.Boolean, default=False, nullable=False)
+    hembra_is_created = db.Column(db.Boolean, default=False, nullable=False)
     cruza_id = db.Column(db.Integer, db.ForeignKey('cruza.id'), nullable=False)
     
+    def __repr__(self):
+        return '<Camada %r>' %self.id
     
+class Macho(db.Model):
+    __tablename__ = 'macho'
+    __table_args__ = {'extend_existing': True}  
+    
+    db.Model.metadata.reflect(db.engine)
+    id = db.Column(db.Integer, primary_key=True)  
+    caja = db.Column(db.String(20), nullable=False)    
+    cepa = db.Column(db.String(20), nullable=False)
+    fecha_nacimiento = db.Column(db.DateTime, default=datetime.today() + relativedelta(days=-28), nullable=False)
+    fecha_destete = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    cantidad = db.Column(db.Integer, default=0, nullable=False)
+    padres = db.Column(db.String(20), nullable=False)
+    cruza_id = db.Column(db.Integer, db.ForeignKey('cruza.id'), nullable=False)    
+    observacion = db.relationship('Observacion', backref='macho', lazy=True)
+    
+    def __repr__(self):
+        return '<Macho %r>' %self.id
+    
+    
+class Hembra(db.Model):
+    __tablename__ = 'hembra'
+    __table_args__ = {'extend_existing': True}  
+    
+    db.Model.metadata.reflect(db.engine)
+    id = db.Column(db.Integer, primary_key=True)  
+    caja = db.Column(db.String(20), nullable=False)    
+    cepa = db.Column(db.String(20), nullable=False)
+    fecha_nacimiento = db.Column(db.DateTime, default=datetime.today() + relativedelta(days=-28), nullable=False)
+    fecha_destete = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    cantidad = db.Column(db.Integer, default=0, nullable=False)
+    padres = db.Column(db.String(20), nullable=False)
+    cruza_id = db.Column(db.Integer, db.ForeignKey('cruza.id'), nullable=False)
+    observacion = db.relationship('Observacion', backref='hembra', lazy=True)
+        
+    def __repr__(self):
+        return '<Hembra %r>' %self.id
+    
+class Observacion(db.Model):
+    __tablename__ = 'observacion'
+    __table_args__ = {'extend_existing': True}  
+    
+    db.Model.metadata.reflect(db.engine)
+    id = db.Column(db.Integer, primary_key=True)  
+    observacion = db.Column(db.Text, nullable=False)    
+    fecha = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    macho_id = db.Column(db.Integer, db.ForeignKey('macho.id'))
+    hembra_id = db.Column(db.Integer, db.ForeignKey('hembra.id'))
+    
+    def __repr__(self):
+        return '<Observacion %r>' %self.id
+    
+#############################PAGINAS#############################################################################   
 @app.route("/", methods=['POST', 'GET'])
 def index():
     db.create_all()
@@ -47,15 +106,14 @@ def index():
         fecha_cruza = datetime.strptime(request.form['fecha_cruza'], "%Y-%m-%d")        
         machos = int(request.form['machos'])        
         hembras = int(request.form['hembras'])        
-        new_cruza = Cruza(caja=caja, cepa=cepa, fecha_cruza=fecha_cruza, machos=machos, hembras=hembras)
-        
+        new_cruza = Cruza(caja=caja, cepa=cepa, fecha_cruza=fecha_cruza, machos=machos, hembras=hembras)        
         
         try:
             db.session.add(new_cruza)
             db.session.commit()
             return redirect('/')
         except:
-            return "Hubo un error agregando la cruza a la base de datos"
+            return redirect("/user-error/")
     else:
         cruzas = Cruza.query.order_by(Cruza.fecha_cruza.desc()).all()
         return render_template("index.html", cruzas=cruzas, cepa_list=cepa_list)
@@ -63,7 +121,7 @@ def index():
 @app.route("/camada/<int:id>", methods=['POST', 'GET'])
 def camada(id):
     cruza = Cruza.query.get_or_404(id)
-    if request.method == 'POST':
+    if request.method == 'POST':        
         fecha_nacimiento = datetime.strptime(request.form['fecha_cruza'], "%Y-%m-%d")      
         fecha_destete = fecha_nacimiento + relativedelta(days=+28)       
         machos = int(request.form['machos'])        
@@ -73,24 +131,119 @@ def camada(id):
         try:
             db.session.add(new_camada)
             db.session.commit()
-            return redirect('/')
+            return redirect('/camada/{}'.format(id))
         except:
-            return "Hubo un error agregando la cruza a la base de datos"
+            return redirect("/user-error/")
     else:        
         return render_template("camada.html", cruza=cruza)
 
+@app.route("/macho/<int:id>", methods=['POST', 'GET'])    
+def macho(id):
+    camada = Camada.query.get_or_404(id)    
+    cruza = Cruza.query.get_or_404(camada.cruza_id)
+            
+    if request.method == 'POST' and camada.macho_is_created == False:
+        caja = request.form['caja'].upper()
+        try:
+            fecha_destete = datetime.strptime(request.form['fecha_destete'], "%Y-%m-%d")
+        except:
+            return redirect("/user-error/")
+        if caja and camada.fecha_destete:
+            padres = cruza.caja
+            macho = Macho(caja=caja, fecha_nacimiento=camada.fecha_nacimiento,fecha_destete=fecha_destete, cantidad=camada.machos, cepa=cruza.cepa, cruza=cruza, padres=padres)      
+                            
+            try:                         
+                db.session.add(macho)
+                db.session.commit()
+                camada.macho_is_created = True
+                db.session.commit()
+                return redirect('/camada/{}'.format(id))
+            except Exception as e:
+                return str(e)
+        else:
+            return redirect("/user-error/")
+    else:
+        return render_template("macho.html", cruza=cruza, camada=camada)
+        
+@app.route("/hembra/<int:id>", methods=['POST', 'GET'])    
+def hembra(id):
+    camada = Camada.query.get_or_404(id)    
+    cruza = Cruza.query.get_or_404(camada.cruza_id)
+    
+    if request.method == 'POST' and camada.hembra_is_created == False:        
+        caja = request.form['caja'].upper()
+        try:
+            fecha_destete = datetime.strptime(request.form['fecha_destete'], "%Y-%m-%d")
+        except:
+            return redirect("/user-error/")
+        if caja and camada.fecha_destete:
+            padres = cruza.caja
+            hembra = Hembra(caja=caja, fecha_nacimiento=camada.fecha_nacimiento,fecha_destete=fecha_destete, cantidad=camada.hembras, cepa=cruza.cepa, cruza=cruza, padres=padres)
+                    
+            try:            
+                db.session.add(hembra)
+                db.session.commit()
+                camada.hembra_is_created = True
+                db.session.commit()            
+                return redirect('/camada/{}'.format(id))
+            except Exception as e:
+                return str(e)
+        else:
+            return redirect("/user-error/")
+    else:
+        return render_template("hembra.html", cruza=cruza, camada=camada)
+    
+    
+@app.route("/macho-hembra/", methods=['GET'])    
+def macho_hembra_get():    
+    machos = Macho.query.order_by(Macho.cepa).all()
+    hembras = Hembra.query.order_by(Hembra.cepa).all()
+    return render_template("macho-hembra.html", machos=machos, hembras=hembras)
+
+@app.route("/observacion-macho/<int:id>", methods=['POST', 'GET'])
+def observacion_macho(id):
+    macho = Macho.query.get_or_404(id)
+    if request.method == 'POST':        
+        observacion =  request.form['observacion']            
+         
+        new_observacion = Observacion(observacion=observacion, macho=macho)
+                
+        try:
+            db.session.add(new_observacion)
+            db.session.commit()
+            return redirect('/macho-hembra/')
+        except:
+            return redirect("/user-error/")
+    else:        
+        return render_template("observacion-macho.html", macho=macho)
+    
+@app.route("/observacion-hembra/<int:id>", methods=['POST', 'GET'])
+def observacion_hembra(id):
+    hembra = Hembra.query.get_or_404(id)
+    if request.method == 'POST':        
+        observacion =  request.form['observacion']            
+         
+        new_observacion = Observacion(observacion=observacion, hembra=hembra)
+                
+        try:
+            db.session.add(new_observacion)
+            db.session.commit()
+            return redirect('/macho-hembra/')
+        except:
+            return redirect("/user-error/")
+    else:        
+        return render_template("observacion-hembra.html", hembra=hembra)
     
 @app.route('/delete/<int:id>')
 def delete(id):
-    cruza_to_delete = Cruza.query.get_or_404(id)   
-     
+    cruza_to_delete = Cruza.query.get_or_404(id)     
     
     try:
         db.session.delete(cruza_to_delete)
         db.session.commit()
         return redirect('/')
     except:
-        return "There was a problem deleting the task"
+        return redirect("/user-error/")
     
 @app.route('/update/<int:id>', methods=['POST', 'GET'])
 def update(id):    
@@ -107,10 +260,20 @@ def update(id):
             db.session.commit()
             return redirect('/')
         except:
-            return "Hubo un error "
+            return redirect("/user-error/")
         
     else:        
         return render_template('update.html', cruza=cruza, cepa_list=cepa_list)
+    
+@app.route('/user-error/', methods=['GET'])
+def user_error():
+    return render_template('user-error.html')
+    
+    
+@app.route("/destete/", methods=['GET'])
+def destete():
+        camadas = Camada.query.order_by(Camada.fecha_destete.desc()).all()
+        return render_template("destete.html", camadas=camadas)
     
 @app.route('/update-camada/<int:id>', methods=['POST', 'GET'])
 def update_camada(id):    
@@ -126,7 +289,7 @@ def update_camada(id):
             db.session.commit()
             return redirect('/')
         except:
-            return "Hubo un error "
+            return redirect("/user-error/")
          
     else:        
         return render_template('update-camada.html', camada=camada)
