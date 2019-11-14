59bb985f4e9128cab0ff56f44f5daef60eae7352
darkenrule <fernandojesus.gonzalezalonso@gmail.com>
2019-11-14 03:15:44 -0600

    caja status

--- app.py
+++ app.py
@@ -99,21 +99,24 @@ class Observacion(db.Model):
 #############################PAGINAS#############################################################################   
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    db.create_all()
+    db.create_all()    
     if request.method == 'POST':
-        caja = request.form['caja'].upper()        
+        caja = request.form['caja'].upper()
         cepa = request.form['cepa']        
         fecha_cruza = datetime.strptime(request.form['fecha_cruza'], "%Y-%m-%d")        
         machos = int(request.form['machos'])        
         hembras = int(request.form['hembras'])        
         new_cruza = Cruza(caja=caja, cepa=cepa, fecha_cruza=fecha_cruza, machos=machos, hembras=hembras)        
         
-        try:
-            db.session.add(new_cruza)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return redirect("/user-error/")
+        if verificar_caja(caja, cepa) and existe_caja(caja) == False:            
+            try:
+                db.session.add(new_cruza)
+                db.session.commit()
+                return redirect('/')
+            except:
+                return redirect("/user-error/")
+        else:
+            return render_template("error.html", error="El nombre de la caja no coincide con el tipo de la cepa o la caja ya existe.")
     else:
         cruzas = Cruza.query.order_by(Cruza.fecha_cruza.desc()).all()
         return render_template("index.html", cruzas=cruzas, cepa_list=cepa_list)
@@ -151,19 +154,23 @@ def macho(id):
         if caja and camada.fecha_destete:
             padres = cruza.caja
             macho = Macho(caja=caja, fecha_nacimiento=camada.fecha_nacimiento,fecha_destete=fecha_destete, cantidad=camada.machos, cepa=cruza.cepa, cruza=cruza, padres=padres)      
-                            
-            try:                         
-                db.session.add(macho)
-                db.session.commit()
-                camada.macho_is_created = True
-                db.session.commit()
-                return redirect('/camada/{}'.format(id))
-            except Exception as e:
-                return str(e)
+            
+            if verificar_caja(caja, cruza.cepa) and existe_caja(caja) == False:                
+                try:                         
+                    db.session.add(macho)
+                    db.session.commit()
+                    camada.macho_is_created = True
+                    db.session.commit()
+                    return redirect('/camada/{}'.format(id))
+                except Exception as e:
+                    return str(e)
+            else:
+                return render_template("error.html", error="El nombre de la caja no coincide con el tipo de la cepa o la caja ya existe.")
         else:
             return redirect("/user-error/")
     else:
-        return render_template("macho.html", cruza=cruza, camada=camada)
+        caja = siguiente_caja(cruza.cepa)
+        return render_template("macho.html", cruza=cruza, camada=camada, caja=caja)
         
 @app.route("/hembra/<int:id>", methods=['POST', 'GET'])    
 def hembra(id):
@@ -191,7 +198,8 @@ def hembra(id):
         else:
             return redirect("/user-error/")
     else:
-        return render_template("hembra.html", cruza=cruza, camada=camada)
+        caja = siguiente_caja(cruza.cepa)
+        return render_template("hembra.html", cruza=cruza, camada=camada, caja=caja)
     
     
 @app.route("/macho-hembra/", methods=['GET'])    
@@ -267,8 +275,7 @@ def update(id):
     
 @app.route('/user-error/', methods=['GET'])
 def user_error():
-    return render_template('user-error.html')
-    
+    return render_template('user-error.html')    
     
 @app.route("/destete/", methods=['GET'])
 def destete():
@@ -293,6 +300,83 @@ def update_camada(id):
          
     else:        
         return render_template('update-camada.html', camada=camada)
+    
+def verificar_caja(caja, cepa):
+    if cepa == cepa_list[0]:        
+        return "A" == str(caja[:1])
+    if cepa == cepa_list[1]:
+        return "B" == str(caja[:1])
+    if cepa == cepa_list[2]:
+        return "C" == str(caja[:1])
+
+    return False
+
+def regresar_letra_cepa(cepa):
+    if cepa == cepa_list[0]:        
+        return "A"
+    if cepa == cepa_list[1]:
+        return "B"
+    if cepa == cepa_list[2]:
+        return "C"    
+
+def siguiente_caja(cepa):    
+    lista = []
+    machos = Macho.query.order_by(Macho.cepa).all()
+    hembras = Hembra.query.order_by(Hembra.cepa).all()
+    cruzas = Cruza.query.order_by(Cruza.fecha_cruza.desc()).all()
+    
+    letra = regresar_letra_cepa(cepa)
+   
+    for macho in machos:   
+        if letra == macho.caja[:1]:
+            lista.append(macho.caja) 
+        
+    for hembra in hembras:
+        if letra == hembra.caja[:1]:
+            lista.append(hembra.caja) 
+        
+    for cruza in cruzas:
+        if letra == cruza.caja[:1]:
+            lista.append(cruza.caja)
+            
+    values = sorted([int(x[1:]) for x in lista])
+    print(values)
+    
+    numero = 0
+    
+    if len(values) == 0:
+        numero = 1
+    elif len(values) == 1:
+        numero = values[-1] + 1
+    else:        
+        for i in range(len(values)-1):
+            if values[i] + 1 != values[i+1]:
+                numero = values[i] + 1
+                break
+            elif i == len(values) - 2:
+                numero = values[-1] + 1
+            
+    return letra + str(numero)
+
+def existe_caja(caja):
+    lista = []
+    machos = Macho.query.order_by(Macho.cepa).all()
+    hembras = Hembra.query.order_by(Hembra.cepa).all()
+    cruzas = Cruza.query.order_by(Cruza.fecha_cruza.desc()).all()
+    for macho in machos:   
+        if caja == macho.caja:
+            lista.append(macho.caja) 
+        
+    for hembra in hembras:
+        if caja == hembra.caja:
+            lista.append(hembra.caja) 
+        
+    for cruza in cruzas:
+        if caja == cruza.caja:
+            lista.append(cruza.caja)
+            
+    return len(lista) > 0
+    
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
